e8f6318990dec8bfb4d7cb9600e692561bea103a
Jay <swaney1127@gmail.com>
2024-08-20 02:04:43 +0700

    Add files via upload
    Monitor for particular string in log file and take action accordingly

---
+++ monitor.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+tail -Fn0 /ArcSightSmartConnectors/current/logs/agent.out.wrapper.log | \
+while read line ; do
+		echo "$line" | grep -q "ET="
+		
+		if [[ $line =~ "ET=Down" ]];	then
+			echo "$(date)  Forwarding Connector Is Down"
+			/etc/init.d/arc_superagent_ng_Name restart
+			echo "$(date) Restarting  Connector"
+			
+		elif [[ $line =~ "ET=Up" ]]; 	then
+			echo "$(date) Forwarding Connector is Up"
+		fi
+		
+done
