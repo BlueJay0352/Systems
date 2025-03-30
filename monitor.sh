5ca39213724317813140acfd46141d92c309001a
Jay <swaney1127@gmail.com>
2025-03-30 15:31:11 +0700

    Update monitor.sh

--- monitor.sh
+++ monitor.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+
+# Monitors a log file for specific entries and performs action.  
+
 tail -Fn0 /ArcSightSmartConnectors/current/logs/agent.out.wrapper.log | \
 while read line ; do
 		echo "$line" | grep -q "ET="
