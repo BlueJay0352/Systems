c912b3d61055a41d16771c542411c7746b7492df
Jay <swaney1127@gmail.com>
2025-03-30 15:41:04 +0700

    Update monitor.sh

--- monitor.sh
+++ monitor.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-# Monitors a log file for specific entries and performs action.  
+# Monitors a log file for specific entries, performs actions, and outputs to log file
 
 tail -Fn0 /ArcSightSmartConnectors/current/logs/agent.out.wrapper.log | \
 while read line ; do
 		echo "$line" | grep -q "ET="
 		
 		if [[ $line =~ "ET=Down" ]];	then
-			echo "$(date)  Forwarding Connector Is Down"
+			echo "$(date)  Forwarding Connector Is Down" >> /var/log/connector_monitor.log
 			/etc/init.d/arc_superagent_ng_Name restart
-			echo "$(date) Restarting  Connector"
+			echo "$(date) Restarting  Connector" >> /var/log/connector_monitor.log
 			
 		elif [[ $line =~ "ET=Up" ]]; 	then
-			echo "$(date) Forwarding Connector is Up"
+			echo "$(date) Forwarding Connector is Up" >> /var/log/connector_monitor.log
 		fi
 		
 done
